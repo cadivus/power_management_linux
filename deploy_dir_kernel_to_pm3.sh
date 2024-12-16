b228a987317709777e11e5707922d75daf572191
Jonas Greifenhain <cadivus@daverkomp.de>
2024-12-16 10:14:49 +0100

    Move kernel to its own subdir
    Gives it a better structure when having modules and tools

--- deploy_dir_kernel_to_pm3.sh
+++ deploy_dir_kernel_to_pm3.sh
@@ -20,9 +20,9 @@ if ! git diff --quiet || ! git diff --cached --quiet; then
 fi
 
 build_start_time=$(date +%s)
-rsync -avz --delete-during -e "ssh -J pm3@lab.os.itec.kit.edu" "$scriptdir/" pm3@pm3:/unser_zeug/git/deploy_script/repo/power_management_linux
+rsync -avz --delete-during -e "ssh -J pm3@lab.os.itec.kit.edu" "$scriptdir/linux/" pm3@pm3:/unser_zeug/git/deploy_script/repo/power_management_linux/linux
 echo "Build on pm3:"
-ssh -J pm3@lab.os.itec.kit.edu pm3@pm3 "cd /unser_zeug/git/deploy_script/repo/power_management_linux && make -j20 LOCALVERSION=-$githash"
+ssh -J pm3@lab.os.itec.kit.edu pm3@pm3 "cd /unser_zeug/git/deploy_script/repo/power_management_linux/linux && make -j20 LOCALVERSION=-$githash"
 end_time=$(date +%s)
 runtime=$((end_time - build_start_time))
 minutes=$((runtime / 60))
@@ -31,10 +31,10 @@ echo "Build runtime: ${minutes} minutes and ${seconds} seconds"
 
 echo ""
 echo "Install modules on pm3 (root password will be autotyped):"
-ssh -J pm3@lab.os.itec.kit.edu pm3@pm3 "bash -c 'cd /unser_zeug/git/deploy_script/repo/power_management_linux && sudo -S <<< \"pm3\" make modules_install -j20 LOCALVERSION=-$githash'"
+ssh -J pm3@lab.os.itec.kit.edu pm3@pm3 "bash -c 'cd /unser_zeug/git/deploy_script/repo/power_management_linux/linux && sudo -S <<< \"pm3\" make modules_install -j20 LOCALVERSION=-$githash'"
 echo ""
 echo "Install kernel on pm3 (root password will be autotyped):"
-ssh -J pm3@lab.os.itec.kit.edu pm3@pm3 "bash -c 'cd /unser_zeug/git/deploy_script/repo/power_management_linux && sudo -S <<< \"pm3\" make install -j20 LOCALVERSION=-$githash'"
+ssh -J pm3@lab.os.itec.kit.edu pm3@pm3 "bash -c 'cd /unser_zeug/git/deploy_script/repo/power_management_linux/linux && sudo -S <<< \"pm3\" make install -j20 LOCALVERSION=-$githash'"
 echo "Done, you can now reboot the machine."
 
 end_time=$(date +%s)
