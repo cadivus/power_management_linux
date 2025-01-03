3d3cf9a45e271d8c8aa1e1f4e055bb6a2ab41aa1
Thomas Oltmann <usjre@student.kit.edu>
2025-01-03 19:18:52 +0100

    purge script: added more delete paths

--- purge_kernel_revision_on_pm3.sh
+++ purge_kernel_revision_on_pm3.sh
@@ -8,7 +8,8 @@ for revision in "$@"; do
 	purge_cmds="$(cat <<-END
 	rm -f "/boot/initramfs-$revision.img"
 	rm -f "/boot/vmlinuz-$revision"
-	rm -rf "/usr/lib/modules-$revision/"
+	rm -f "/boot/loader/entries/*-$revision.conf"
+	rm -rf "/usr/lib/modules/$revision/"
 	rm -rf "/usr/src/kernels/$revision/"
 	END
 	)"
@@ -16,3 +17,8 @@ for revision in "$@"; do
 	#printf "%s\n" "$purge_cmds"
 	ssh -J pm3@lab.os.itec.kit.edu pm3@pm3 "bash -c \"sudo -S <<< \"pm3\" sh -c '$purge_cmds'\""
 done
+
+# Supposedly it is not neccessary to update grub, as it auto-detects installed kernels?
+#printf "Updating GRUB2 menu entries\n"
+#ssh -J pm3@lab.os.itec.kit.edu pm3@pm3 "bash -c \"cd / && sudo -S <<< \"pm3\" grub2-mkconfig -o /boot/grub2/grub.cfg\""
+
