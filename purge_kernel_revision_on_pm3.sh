d509faaf0e5a00ee76e83b00c0f0a123b5a180dc
Thomas Oltmann <usjre@student.kit.edu>
2025-01-16 01:01:07 +0100

    Fixed early glob eval in purge script

--- purge_kernel_revision_on_pm3.sh
+++ purge_kernel_revision_on_pm3.sh
@@ -6,16 +6,20 @@ for revision in "$@"; do
 	printf "Purging %s\n" "$revision"
 
 	purge_cmds="$(cat <<-END
-	rm -f "/boot/initramfs-$revision.img"
-	rm -f "/boot/vmlinuz-$revision"
-	rm -f "/boot/loader/entries/*-$revision.conf"
-	rm -rf "/usr/lib/modules/$revision/"
-	rm -rf "/usr/src/kernels/$revision/"
+	{
+	rm -f "/boot/initramfs-$revision.img";
+	rm -f "/boot/vmlinuz-$revision";
+	rm -f "/boot/loader/entries/*-$revision.conf";
+	rm -rf "/usr/lib/modules/$revision/";
+	rm -rf "/usr/src/kernels/$revision/";
+	}
 	END
 	)"
 
-	#printf "%s\n" "$purge_cmds"
-	ssh -J pm3@lab.os.itec.kit.edu pm3@pm3 "bash -c \"sudo -S <<< \"pm3\" sh -c '$purge_cmds'\""
+	purge_cmds="echo pm3 | sudo -S sh -c '$purge_cmds'"
+
+	printf "EXECUTING: %s\n" "$purge_cmds"
+	ssh -J pm3@lab.os.itec.kit.edu pm3@pm3 $purge_cmds
 done
 
 # Supposedly it is not neccessary to update grub, as it auto-detects installed kernels?
